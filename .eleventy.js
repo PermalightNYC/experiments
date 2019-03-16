b21cce8b107e029f35ee08532f55cc36b568c24e
Dan Leatherman <dleatherman@gmail.com>
2019-03-16 16:07:07 -0400

    correct order

--- .eleventy.js
+++ .eleventy.js
@@ -6,14 +6,8 @@ module.exports = function (config) {
   config.addFilter("timestamp", require("./filters/timestamp.js"));
   config.addFilter("squash", require("./filters/squash.js"));
 
-
-  config.addPassthroughCopy('img');
-  config.addPassthroughCopy('css');
-  config.addPassthroughCopy('js');
-
   config.addCollection('experiments', function (collection) {
-    return collection.getAll().reverse().filter(function (item) {
-      // console.log('status' in item.data);
+    return collection.getAllSorted().reverse().filter(function (item) {
       if ('status' in item.data == true) {
         return item.data
       }
