30f2f5f0df9370b48c736ccfe288a343f8be230f
Dan Leatherman <dleatherman@gmail.com>
2024-12-05 16:32:13 -0500

    cat loader

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,12 @@
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
-
   // Add a date formatter filter to Nunjucks
   eleventyConfig.addFilter("dateDisplay", require("./filters/dates.js"));
   eleventyConfig.addFilter("timestamp", require("./filters/timestamp.js"));
   eleventyConfig.addFilter("squash", require("./filters/squash.js"));
 
-  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy("css");
 
   // Minify CSS
   eleventyConfig.addFilter("cssmin", function (code) {
@@ -16,24 +15,39 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
-  eleventyConfig.addCollection('experiments', function (collection) {
-    return collection.getAllSorted().reverse().filter(function (item) {
-      if ('status' in item.data == true) {
-        return item.data
-      }
-    });
+  eleventyConfig.addCollection("experiments", function (collection) {
+    return collection
+      .getAllSorted()
+      .reverse()
+      .filter(function (item) {
+        if ("status" in item.data == true) {
+          return item.data;
+        }
+      });
   });
 
   return {
     dir: {
       input: "src/site",
       output: "dist",
-      includes: "_includes"
+      includes: "_includes",
     },
-    templateFormats: ["njk", "md", "png", "ico", "pdf", "css", "js", "wav", "mp3", "gltf", "glb"],
+    templateFormats: [
+      "njk",
+      "md",
+      "png",
+      "ico",
+      "pdf",
+      "css",
+      "js",
+      "wav",
+      "mp3",
+      "gltf",
+      "glb",
+      "json",
+    ],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
   };
-
 };
